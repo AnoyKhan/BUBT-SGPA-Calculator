e57419d5da200c81e2c4cffd20d3f527a158717f
AnoyKhan <anoykhan14@gmail.com>
2026-05-07 14:56:40 +0600

    Build and deploy React app - dist and docs folders synchronized

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/BUBT-SGPA-Calculator/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true
-  },
   server: {
     port: 3000
   }
