86b7074e90e6dd02563dceabf5eca2e804b8bec3
AnoyKhan <anoykhan14@gmail.com>
2026-05-14 20:09:52 +0600

    logo error fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <header>
-      <div className="header-icon">
-        <img 
-          src="/bubt-logo-png_seeklogo-498306.png" 
-          alt="BUBT Logo" 
-          style={{ width: "40px", height: "auto" }} // Logo-ti control korar jonno width add kora hoyeche
-        />
-      </div>
+      <div className="header-icon">📊</div>
       <h1>SGPA Calculator</h1>
       <p>Calculate your semester SGPA instantly</p>
     </header>
